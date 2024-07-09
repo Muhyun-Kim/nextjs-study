8d8a2077bb1829cc47d6ec66c56411d9aa5faca4
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-07-09 14:00:39 +0900

    get products

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,16 +1,26 @@
+import ListProduct from "@/components/list-product";
+import db from "@/lib/db";
+
 async function getProduct() {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Product");
-    }, 2000);
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
   });
+  return products;
 }
 
 export default async function Product() {
-  const product = await getProduct();
+  const products = await getProduct();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Product!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
